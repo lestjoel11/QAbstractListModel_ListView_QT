258d62fa1e93c7af008c3b5d4aa17a888a8fc896
lestjoel11 <lestjoel@gmail.com>
2023-04-14 17:42:12 +0400

    update to handle new projects, add cors for wasm

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const cors = require("cors")
+
 const port = 3000;
 const dataPath = "../Data.json";
 const jsonData = JSON.parse(fs.readFileSync(dataPath));
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:30000', 'http://localhost:3000']
+  }));
 
 const rangedData = (start, end, jsonData = JSON.parse) => {
     const filteredData = () => {
@@ -12,6 +19,19 @@ const rangedData = (start, end, jsonData = JSON.parse) => {
     console.log(filteredData().length);
     return filteredData();
 };
+
+app.get("/itemCount", (req, res) => {
+    const itemCount = {"itemCount":jsonData.length}
+    res.json(itemCount);
+});
+
+app.get("/", (req, res) => {
+    // const start = req.params.range.split("-")[0];
+    // const end = req.params.range.split("-")[1];
+    // const result = rangedData(start, end, jsonData);
+    console.log("jsonData")
+    res.json(jsonData);
+});
 
 app.get("/:range", (req, res) => {
     const start = req.params.range.split("-")[0];
